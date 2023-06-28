46d0c75792b58b2f87e4f5deba6f873f4d2d30cf
happynel <frankyhappy5050@gmail.com>
2023-06-28 11:08:27 +0000

    Let me in

--- 0x0B-ssh/let_me_in.sh
+++ 0x0B-ssh/let_me_in.sh
@@ -5,7 +5,8 @@ server=54.152.183.141
 user="ubuntu"
 
 # SSH public key
-ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHYcTDMUfN3lJi/lUcICtWR5XrMIy/6NFaSMRJAqmfoEcs6lhP/eow2CrYwwLK/e/iPKKUA5bebUZv+O9RV/E+jJzVsGH1FJpkAvV4Iu305NnOeNJmBKyQcwfKDTYNB+N79BnQbXmw7drRliE/DRaMqZhhGL/ef25kKJqCgPsm8cq5RxbZUZljjOLQzZH5vzp3ruDomZNUjwIs/fLnIjLUr7M7v2v6tT8IMKNvpq15aLifPeciRmh8e/nChTtej/AU+74xiZBDJyCywjcVsQFvXXtyzQHXLbiboqY4okSmSu8Wa3OKn/FZfOqdAOS/g1ItSMxaKoc7zCEFuaWohNGroo57LMgV/9ZvHTaEMsPigcBNaZj0l98v4a9jULU3if89QcgDFDU0eQif8OUFk0dIsFh4cp1h7gDFLsU/LzbEzAaUCcWzBiPouJCkl8cp5uCZqa+h3aGy1AWreptG/cHvSVNhUrRTt+q4N7lpy3AlBxVkVMwYEbb38lucv8gmhwOZwZ1s3UgFwKYAgt3dIc5+Rk3XFUpYi6FfeJxICM/xjdT4Tf1p8uPrxrFbriBKtzInkFlJqqBmLEX8XeVjiIbstueRyu2FDF8hFUqspj3n12Ag+GXLEAzqr3wmRedDkr0vWf/BgNGh5c+x9oBTnNvxtsFGT009vds/AT22vOitoQ== frankyhappy5050@gmail.com"
+ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN"
+
 # Connect to the server and add the SSH public key to authorized_keys
 ssh ubuntu@54.152.183.141 "mkdir -p ~/.ssh && echo \"$ssh_public_key\" >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys"
 
